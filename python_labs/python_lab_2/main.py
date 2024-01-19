2aea35182ad01013b258be7689cb7adda3cca290
Tyler Savage <thatrealtyguy@gmail.com>
2024-01-19 15:29:19 -0700

    Filled out main function

--- python_labs/python_lab_2/main.py
+++ python_labs/python_lab_2/main.py
@@ -6,9 +6,23 @@ import os
 def main(inputString):
     # Write the code to count the number of words here
     # Remember to save the dictionary as a json file named "word-counts.json"
+    inputString = inputString.replace(string.punctuation, '')
+    inputString = inputString.lower()
+    output_dict = {'notaword': 1}
 
+    space_bool = True
+    while space_bool:
+        space_index = inputString.find(' ')
+        word = inputString[:space_index]
+        if output_dict.get(word, 0) == 0:
+            output_dict[word] =  1
+        else:
+            output_dict[word] = output_dict[word] + 1
+        space_index = space_index + 1
+        inputString = inputString[space_index:]
 
-    return None
+    del output_dict['notaword']
+    return output_dict
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Word Counter")
