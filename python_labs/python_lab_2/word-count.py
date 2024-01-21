8ae583414ef0ef1b42357a47b27a1c1f9b26e8f3
Tyler Savage <thatrealtyguy@gmail.com>
2024-01-20 21:05:34 -0700

    Updated word-count.py with json writing

--- python_labs/python_lab_2/word-count.py
+++ python_labs/python_lab_2/word-count.py
@@ -6,32 +6,49 @@ import os
 def main(inputString):
     # Write the code to count the number of words here
     # Remember to save the dictionary as a json file named "word-counts.json"
+
+    # Remove unnecessary characters
     inputString = inputString.translate(str.maketrans('', '', string.punctuation))
     inputString = inputString.lower()
 
+    # Initialize variables
     output_dict = {'notaword': 1}
     space_bool = True
     temp_len = 0
 
+    # Write to dictionary
     while space_bool:
         temp_len = len(inputString)
         space_index = inputString.find(' ')
+
+        # Check for end of string and get word
         if space_index < 0:
             word = inputString
         else:
             word = inputString[:space_index]
+
+        # Add word to dictionary or increment count
         if output_dict.get(word, 0) == 0:
             output_dict[word] =  1
         else:
             output_dict[word] = output_dict[word] + 1
+
+        # Remove word from string for next loop
         space_index = space_index + 1
         inputString = inputString[space_index:]
+
+        # Check for end of string
         if temp_len == len(inputString):
             space_bool = False
 
+    # Remove initializing word
     del output_dict['notaword']
-    print(output_dict)
-    return output_dict
+    
+    # Write dictionary to json file
+    with open('word-counts.json', 'w') as f:
+        json.dump(output_dict, f)
+
+    return f
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Word Counter")
