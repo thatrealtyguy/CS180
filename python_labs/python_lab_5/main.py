c08823904a08cc5d838676523af5ddf652ba58b4
Tyler Savage <thatrealtyguy@gmail.com>
2024-02-02 15:50:52 -0700

    main.py finished, ready for testing and renaming

--- python_labs/python_lab_5/main.py
+++ python_labs/python_lab_5/main.py
@@ -3,8 +3,12 @@ import argparse
 def main(inputString):
     # Write the code to reverse the string here
     # Make sure that your terminal output matches the terminal output of the example given on the instructions.
-    
-    return None
+    output = ''
+
+    for char in inputString[::-1]:
+        output = output + char
+
+    return output
 
 if __name__ == "__main__":
     argumentParser = argparse.ArgumentParser("String Reverser")
