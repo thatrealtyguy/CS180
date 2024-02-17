0c2d388825837c2afcbdbed4397616cc01539c2c
Tyler Savage <thatrealtyguy@gmail.com>
2024-02-17 16:58:07 -0700

    Finished Lab

--- python_labs/python_lab_6/main.py
+++ python_labs/python_lab_6/main.py
@@ -3,7 +3,26 @@ import argparse
 def main(number):
     # Write the code to determine whether or not a number is a pallindrome here.
     # Make sure that your terminal output matches the terminal output of the example given on the instructions.
+    if number < 0:
+        print(False)
+        return None
     
+    num_str = str(number)
+
+    if len(num_str) == 1:
+        print(True)
+        return None
+    else:
+        half_index = int(round(len(num_str) / 2))
+        index = -1
+        for char in num_str[:half_index]:
+            end_char = num_str[index]
+            if char != end_char:
+                print(False)
+                return None
+            index -= 1
+
+    print(True)
     return None
 
 if __name__ == "__main__":
